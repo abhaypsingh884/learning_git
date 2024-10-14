c9ce4fd08e63271b99d2ba7aef1506200ce68823
abhay <abhaypsingh884@gmail.com>
2024-10-14 22:55:09 +0530

    js api is created

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const a=12;
+
+console.log("abhay ");
+
+const populate = async (currency, value) => {
+    myStr = "";
+
+    url = `https://api.currencyapi.com/v3/latest?apikey=cur_live_k6TOv268URRffqW92b0OHjAWCR8KRcOqTGSsagDm&base_currency=${currency}`;
+    let res = await fetch(url);
+    let response = await res.json();
+    for (let key of Object.keys(response.data)) {
+        myStr += `
+    <tr>
+
+    <td>${key}</td>
+    <td>${response["data"][key]["code"]}</td>
+    <td>${Math.round(response["data"][key]["value"] * value)}</td>
+</tr> `
+    }
+
+    const tablebody = document.querySelector("tbody");
+    tablebody.innerHTML = myStr;
+
+}
+
+btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    console.log("hii")
+    e.preventDefault();
+    const currency = document.querySelector('select[name="your_Currency"]').value;
+    const quantity = parseInt(document.querySelector('input[name="your_Quantity"]').value);
+    populate(currency, quantity);
+
+});
+
+
